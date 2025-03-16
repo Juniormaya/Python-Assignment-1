e0e73ed71a47fd3a03ea541a711ccec91e40073e
Juniormaya <joyokwirri@gmail.com>
2025-03-16 17:27:11 -0500

    Calc complete

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+#User input
+num1 = float(input("Enter the first value:"))
+num2 = float(input("Enter the second value:"))
+operation = input("Enter the operation(*):")
+
+#Perform calculation
+if operation == '*':
+    result = num1 * num2
+
+#Display result
+print("Result:", result)
